3447b0b2b1f5352caf92bd6d10ef0511503a3650
Nazarí González <nazari.nz@gmail.com>
2015-09-02 18:34:48 +0100

    check for compatible versions of pixi.js

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,13 @@
 if(!PIXI){
     throw new Error('Ey! Where is pixi.js??');
 }
+
+const PIXI_VERSION_REQUIRED = "3.0.7";
+
+if(getVersion(PIXI.VERSION) < PIXI_VERSION_REQUIRED){
+    throw new Error("Pixi.js v" + PIXI.VERSION + " it's not supported, please use ^" + PIXI_VERSION_REQUIRED);
+}
+
+function getVersion(stringVersion:string):string {
+   return stringVersion.match(/\d.\d.\d/)[0];
+}
