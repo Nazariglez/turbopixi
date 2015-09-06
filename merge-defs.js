8aaacf7f86a1e638097fe3b076e69d9395c5ed5f
Nazarí González <nazari.nz@gmail.com>
2015-09-06 20:22:58 +0100

    added a simple script to merge pix definitions and turbopixi definitions in only one definitions file.

---
+++ merge-defs.js
@@ -0,0 +1,70 @@
+var fs = require('fs'),
+    path = require('path');
+
+var intro = "declare module PIXI {";
+var outro = "\n}" +
+        "\n\n" +
+        "declare module 'pixi.js' {" +
+        "\n    export = PIXI;" +
+        "\n}";
+
+function merge(files){
+    var regex = /\s*(declare)?\s*module\s?PIXI\s*\{/g;
+
+    var content = [];
+    files.forEach(function(file){
+        var dir = path.normalize(__dirname + '/' + file);
+        content.push(fs.readFileSync(dir, 'utf8').split('\n'));
+    });
+
+    var definitions = "";
+    for(var i = 0; i < content.length; i++){
+        var f = content[i];
+        for(var l = 0; l < f.length; l++){
+            var match = f[l].match(regex);
+            if(match){
+                definitions += getBlock(f,l);
+            }
+        }
+    }
+
+    fs.writeFileSync('./bin/turbopixi.test.d.ts', intro + definitions + outro, "utf8", function(err, data){
+        if(err){
+            return console.log(err);
+        }
+    });
+}
+
+function getBlock(file, line){
+    var block = "";
+    var braces = 1;
+
+    for(var l = line+1; l < file.length; l++){
+        var open = file[l].match(/\{/g);
+        var close = file[l].match(/\}/g);
+        braces += (open) ? open.length : 0;
+        braces -= (close) ? close.length : 0;
+
+        if(!braces){
+            break;
+        }
+
+        if(braces >= 1){
+            var spaces = getSpaces(braces - ((open) ? 1 : 0));
+            block += "\n" + spaces + (file[l]).trim();
+        }
+    }
+
+    return block;
+}
+
+function getSpaces(num){
+    var spaces = "";
+    for(var i = 0; i < num; i++)spaces += "  ";
+    return spaces;
+}
+
+merge([
+    "./defs/pixi.js.d.ts",
+    "./bin/turbopixi.d.ts"
+]);
