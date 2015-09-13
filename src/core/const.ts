549496adbd367b2fd7a1d42e4b6e451f220bbb23
Nazarí González <nazari.nz@gmail.com>
2015-09-13 17:44:58 +0100

    moved constants to their own file

---
+++ src/core/const.ts
@@ -0,0 +1,14 @@
+module PIXI {
+    export enum GAME_SCALE_TYPE {
+        NONE,
+        FILL,
+        ASPECT_FIT,
+        ASPECT_FILL
+    }
+
+    export enum AUDIO_TYPE {
+        UNKNOWN,
+        WEBAUDIO,
+        HTMLAUDIO
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -9,18 +9,3 @@ const PIXI_VERSION = PIXI.VERSION.match(/\d.\d.\d/)[0];
 if(PIXI_VERSION < PIXI_VERSION_REQUIRED){
     throw new Error("Pixi.js v" + PIXI.VERSION + " it's not supported, please use ^" + PIXI_VERSION_REQUIRED);
 }
-
-module PIXI {
-    export enum GAME_SCALE_TYPE {
-        NONE,
-        FILL,
-        ASPECT_FIT,
-        ASPECT_FILL
-    }
-
-    export enum AUDIO_TYPE {
-        UNKNOWN,
-        WEBAUDIO,
-        HTMLAUDIO
-    }
-}
